256cf3734246b44305b0c3de47ce867a22e9a643
Bruno Trindade <bruno.t@lyncas.net>
2022-02-16 16:33:50 -0300

    Correcoes script

--- database/ProjectLyncas.sql
+++ database/ProjectLyncas.sql
@@ -23,18 +23,24 @@ CREATE TABLE auths(
     ON DELETE CASCADE
     ON UPDATE CASCADE,
     senha VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL    
+    status BIT NOT NULL    
 )
 
 INSERT INTO pessoas (nome, sobrenome, email, telefone, dataNasc)
 VALUES
-  ('Bruno', 'Trindade', 'bruno@mail.com', '27999009999', '1996-04-01'),
+  ('Bruno', 'Trindade', 'bruno@mail.com', '27999009999', '1996-04-01');
+
+INSERT INTO auths (idPessoa, senha, status)
+VALUES
+  (@@IDENTITY, '123456', 1);
+
+INSERT INTO pessoas (nome, sobrenome, email, telefone, dataNasc)
+VALUES
   ('Gabriela', 'Paganini', 'gabriel@mail.com', '27999779999', '1991-01-03');
 
 INSERT INTO auths (idPessoa, senha, status)
 VALUES
-  (1, '123456', 'true'),
-  (2, '123abc', 'true');
+  (@@IDENTITY, '123abc', 1);
 
 
 -- QUERY COM SELECT DEVOLVENDO TODOS OS USUÁRIOS ATIVOS
@@ -44,11 +50,11 @@ au.status AS status
 FROM pessoas AS ps
 INNER JOIN auths AS au
 ON ps.idPessoa = au.idPessoa
-WHERE au.status = 'true';
+WHERE au.status = 0;
 
 -- QUERY DE UPDATE NA TABELA
 UPDATE ProjectLyncas.dbo.auths
-SET auths.status = 'false'
+SET auths.status = 0
 WHERE auths.idPessoa = 2;
 
 -- QUERY DE DELETE NA TABELA
