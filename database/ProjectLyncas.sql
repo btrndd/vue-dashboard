d27a1d7c8d256e944ca06a05a2d6b541422df725
Bruno Trindade <bruno.t@lyncas.net>
2022-02-22 16:50:46 -0300

    Ajustes CSS frontend

--- database/ProjectLyncas.sql
+++ database/ProjectLyncas.sql
@@ -7,56 +7,56 @@ CREATE DATABASE ProjectLyncas;
 
 USE ProjectLyncas;
 
-CREATE TABLE pessoas(
-    idPessoa INT IDENTITY(1,1) PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    sobrenome VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    telefone VARCHAR(50) NOT NULL,
-    dataNasc DATE NOT NULL
+CREATE TABLE Users(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(60) NOT NULL,
+    LastName VARCHAR(60) NOT NULL,
+    Email VARCHAR(60) NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    BirthDate DATE NOT NULL
 )
 
-CREATE TABLE auths(
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    idPessoa INT NOT NULL
-    FOREIGN KEY REFERENCES pessoas(idPessoa)
+CREATE TABLE Auths(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    UserId INT NOT NULL
+    FOREIGN KEY REFERENCES Users(Id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-    senha VARCHAR(50) NOT NULL,
-    status BIT NOT NULL    
+    Password VARCHAR(MAX) NOT NULL,
+    Status BIT NOT NULL    
 )
 
-INSERT INTO pessoas (nome, sobrenome, email, telefone, dataNasc)
+INSERT INTO Users (Name, LastName, Email, Phone, BirthDate)
 VALUES
   ('Bruno', 'Trindade', 'bruno@mail.com', '27999009999', '1996-04-01');
 
-INSERT INTO auths (idPessoa, senha, status)
+INSERT INTO Auths (UserId, Password, Status)
 VALUES
   (@@IDENTITY, '123456', 1);
 
-INSERT INTO pessoas (nome, sobrenome, email, telefone, dataNasc)
+INSERT INTO Users (Name, LastName, Email, Phone, BirthDate)
 VALUES
   ('Gabriela', 'Paganini', 'gabriel@mail.com', '27999779999', '1991-01-03');
 
-INSERT INTO auths (idPessoa, senha, status)
+INSERT INTO Auths (UserId, Password, Status)
 VALUES
   (@@IDENTITY, '123abc', 1);
 
 
 -- QUERY COM SELECT DEVOLVENDO TODOS OS USUÁRIOS ATIVOS
-SELECT ps.nome AS usuario,
-ps.email AS email,
-au.status AS status
-FROM pessoas AS ps
-INNER JOIN auths AS au
-ON ps.idPessoa = au.idPessoa
-WHERE au.status = 0;
+SELECT ps.Name AS Usuario,
+ps.Email AS Email,
+au.Status AS Status
+FROM Users AS ps
+INNER JOIN Auths AS au
+ON ps.Id = au.UserId
+WHERE au.Status = 0;
 
 -- QUERY DE UPDATE NA TABELA
-UPDATE ProjectLyncas.dbo.auths
-SET auths.status = 0
-WHERE auths.idPessoa = 2;
+UPDATE ProjectLyncas.dbo.Auths
+SET Auths.Status = 0
+WHERE Auths.UserId = 2;
 
 -- QUERY DE DELETE NA TABELA
-DELETE FROM ProjectLyncas.dbo.pessoas
-WHERE pessoas.sobrenome = 'Trindade';
+DELETE FROM ProjectLyncas.dbo.Users
+WHERE pessoas.LastName = 'Trindade';
