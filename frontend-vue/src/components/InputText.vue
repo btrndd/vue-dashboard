e97af1357b279f6d3488184d2e32187e736a6270
Bruno Trindade <bruno.t@lyncas.net>
2022-04-23 11:36:35 -0300

    feat: cria componentes input e login view

---
+++ frontend-vue/src/components/InputText.vue
@@ -0,0 +1,24 @@
+<template>
+  <input name="name" class="form-input" type="type" placeholder="placeholder" />
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: String,
+    type: String,
+    name: String,
+  }
+}
+</script>
+
+<style scoped>
+  .form-input {
+    width: 350px;
+    height: 30px;
+    border-radius: 3px;
+    border: 1px solid lightgray;
+    margin-top: 15px;
+    padding: 5px 10px 5px 10px;
+  }
+</style>
---
+++ frontend-vue/src/components/SubmitBtn.vue
@@ -0,0 +1,24 @@
+<template>
+  <button id="id" type="button">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    text: String,
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    width: 350px;
+    height: 40px;
+    border-radius: 50px;
+    margin-top: 15px;
+    color: white;
+    background-color: #00174a;
+    font-weight: 600;
+  }
+</style>
